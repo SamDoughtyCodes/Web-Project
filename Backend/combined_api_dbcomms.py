519d02b3af7e51ce3ec8149ce1fdd9519da10fd5
SamDoughtyCodes <samdcodes@gmail.com>
2025-06-05 22:07:42 +0100

    YESSSS IT WORKS (the frontend and the db are in kahoots!) (the import issue is still a thing but we just put it all in one file) (#holyairball)

---
+++ Backend/combined_api_dbcomms.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware
+from mysql.connector import connect
+
+app = FastAPI()  # Set up the app section of the program
+
+# Enforce rules on connections
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # What sites can access (the host of the website)
+    allow_credentials=True,
+    allow_methods=["*"],  # What requests (e.g. GET, POST) can be accepted, * represents all
+    allow_headers=["*"],
+)
+
+# Connect to the server
+db = connect(
+    host="localhost",
+    user="py_script",
+    password="py_db_050722"
+)
+cursor = db.cursor()
+
+def set_db(db_name: str):
+    """
+    Function to set the in use database
+
+    param db_name: The name of the database to set.
+    """
+    query = "USE " + db_name + ";"
+    cursor.execute(query)
+
+def run_query(query: str):
+    """
+    Function to send and run a query and return the result
+
+    param query: the SQL query to run
+    returns result: the result of the SQL query
+    """
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return result
+
+# Requests ending "/api/test" will call this function, which returns PlainText
+@app.get("/api/test", response_class=PlainTextResponse)
+def test_func():
+    return "Holy crap this actually works!"
+
+# Request which sends a fixed query to the server for testing, responding with a JSON object
+@app.get("/api/test_db")
+def test_db_conn():
+    set_db("School")
+    result = run_query("SELECT * FROM subjects")
+    return result
